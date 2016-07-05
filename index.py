64ff797b01385e0ac1b48ec13d26a832bb75c912
Sumit Pundir <sumit.rana24@gmail.com>
2016-07-05 08:01:07 -0700

    Update index.py

--- index.py
+++ index.py
@@ -7,7 +7,8 @@ import glob
 cd=ColorDescriptor((8,12,3))
 
 output=open('output1.csv', 'w+')
-for imagepath in glob.glob('D:/image processing/image-search-engine/testimages/*'):
+"""image folder location in pc"""
+for imagepath in glob.glob('image-search-engine/testimages/*'):
     imageID=imagepath[imagepath.rfind("/")+1:]
     image=cv2.imread(imagepath)
     
